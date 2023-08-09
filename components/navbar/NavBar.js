eb85b791f1bfe9b623228ecbf8c92fa6b3c95672
abdelhak-org <abdelhak.org@gmail.com>
2023-08-09 20:58:44 +0200

    m

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,5 +1,5 @@
-"use client"
-import { useEffect, useState } from "react";
+"use client";
+import {  useState } from "react";
 import ListItems from "./ListItems";
 import SocialLinks from "./SocialLinks";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
@@ -7,28 +7,40 @@ import Logo from "./Logo";
 const NavBar = () => {
   const [show, setShow] = useState(true);
   
+
+  
   return (
     <>
       <div
         onClick={() => setShow(!show)}
-        className=" md:hidden my-2 mx-auto text-center text-2xl p-4 text-white cursor-pointer fixed top-4 right-4 z-30 "
-        >
-
-      {!show ? <AiOutlineMenu /> : <AiOutlineClose />}
+        className=" md:hidden my-2 mx-auto text-center text-3xl p-4 text-white bg-slate-900 cursor-pointer fixed top-4 right-4 z-30 "
+      >
+        {!show ? <AiOutlineMenu /> : <AiOutlineClose />}
       </div>
 
- { show &&  <div
-      className={`${
-        show ? "" : ""
-      } w-screen md:max-w-[1535px] h-[100vh]   md:w-screen md:h-[55px] my-0 py-24 md:py-0  mx-auto flex overflow-hidden
+      {show && (
+        <div
+        onClick={()=>{
+          if(window.innerWidth <= "767"){
+            setShow(false)
+          } else {
+            setShow(true)
+          }
+          
+           
+        }}
+          className={`${
+            show ? "" : ""
+          } w-screen md:max-w-[1535px] h-[100vh]   md:w-screen md:h-[55px] my-0 py-24 md:py-0  mx-auto flex overflow-hidden
       flex-col items-center justify-center border-none  md:flex-row md:justify-between 
       bg-gray-900    `}
-      >
-      <Logo />
-      <ListItems />
-      <SocialLinks />
-    </div>}
-      </>
+        >
+          <Logo />
+          <ListItems />
+          <SocialLinks />
+        </div>
+      )}
+    </>
   );
 };
 
